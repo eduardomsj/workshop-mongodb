a984b813c85f4836dbfd90c7ebea079733d77b87
eduardomsj <eduardomsj@gmail.com>
2018-09-13 06:27:40 -0300

    Obtendo um post por id

---
+++ src/main/java/com/eduardojr/workshopmongodb/services/PostService.java
@@ -0,0 +1,28 @@
+package com.eduardojr.workshopmongodb.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.eduardojr.workshopmongodb.domain.Post;
+import com.eduardojr.workshopmongodb.repository.PostRepository;
+import com.eduardojr.workshopmongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public List<Post> findAll() {
+		return repo.findAll();
+	}
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+}
